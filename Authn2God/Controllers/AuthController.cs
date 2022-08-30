2e08b06f5186b9883dbdf652f628a09e69287da7
j8 <walbhota@gmail.com>
2022-08-30 16:21:05 +0100

    Added Role based Authorization

--- Authn2God/Controllers/AuthController.cs
+++ Authn2God/Controllers/AuthController.cs
@@ -54,7 +54,8 @@ namespace Authn2God.Controllers
         {
             List<Claim> claims = new List<Claim>
             {
-                new Claim(ClaimTypes.Name, user.UserName)
+                new Claim(ClaimTypes.Name, user.UserName),
+                 new Claim(ClaimTypes.Role, "Admin")
             };
 
             var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8
